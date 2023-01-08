c2a58dde0d51c2afca8677e9f0d1a0300836db53
wak <2231751148@qq.com>
2023-01-08 21:21:13 +0800

    start init

---
+++ day_1_8/LeetCode2351.py
@@ -0,0 +1,9 @@
+"""
+@file: LeetCode2351.py
+@author: wak
+@date: 2023/1/8 20:45 
+"""
+
+
+def repeatedCharacter(self, s: str) -> str:
+    return 'a'
---
+++ day_1_8/__init__.py
@@ -0,0 +1,6 @@
+"""
+@file: __init__.py.py
+@author: wak
+@date: 2023/1/8 20:38 
+"""
+print("agri")
